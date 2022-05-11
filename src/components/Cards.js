d2b4bde0d8ac6fbd5b75c51962e618e61c8bd7c9
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-11 07:46:26 -0400

    added a content div and a props.number inside the Card component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 
 function Card (props) {
     return <div className="card">
-
+        <div className="card-content">
+            {props.number}
+        </div>
     </div>
 }
 export default Card;
