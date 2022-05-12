8d203d6962a2ddfc01f2a656e07d9661372de3ff
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-12 09:47:52 -0400

    in ScoreCounter created two divs that have class names 'Card-row-' then row number

--- src/components/ScoreCounter.js
+++ src/components/ScoreCounter.js
@@ -22,6 +22,7 @@ function ScoreCounter (props) {
     }
     const cardOrder = createCardOrder();
     return <div>
+        <div className="Card-row-1">
         <MemoryCard number={cardOrder[0]}></MemoryCard>
         <MemoryCard number={cardOrder[1]}></MemoryCard>
         <MemoryCard number={cardOrder[2]}></MemoryCard>
@@ -29,12 +30,15 @@ function ScoreCounter (props) {
         <MemoryCard number={cardOrder[4]}></MemoryCard>
         <MemoryCard number={cardOrder[5]}></MemoryCard>
         <MemoryCard number={cardOrder[6]}></MemoryCard>
+        </div>
+        <div className="Card-row-2">
         <MemoryCard number={cardOrder[7]}></MemoryCard>
         <MemoryCard number={cardOrder[8]}></MemoryCard>
         <MemoryCard number={cardOrder[9]}></MemoryCard>
         <MemoryCard number={cardOrder[10]}></MemoryCard>
         <MemoryCard number={cardOrder[11]}></MemoryCard>
         <MemoryCard number={cardOrder[12]}></MemoryCard>
+        </div>
     </div>
 }
 export default ScoreCounter;
