62863bbbcf7580700ee638b9dbe56cb06a152b6e
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-12 09:52:55 -0400

    ScoreCounter now has two divs for alligning

--- src/components/ScoreCounter.js
+++ src/components/ScoreCounter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import MemoryCard from './MemoryCard';
+import '../ScoreCounter.css';
 function ScoreCounter (props) {
     function createCardOrder() {
         const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
