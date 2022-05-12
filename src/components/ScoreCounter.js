ab3fca1c4270a85251abe9ef9c36fa40834a9af1
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-12 08:57:52 -0400

    created a function inside ScoreCounter named 'createCardOrder'

--- src/components/ScoreCounter.js
+++ src/components/ScoreCounter.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 function ScoreCounter (props) {
-
+    function createCardOrder() {
+        const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    }
 }
 export default ScoreCounter;
