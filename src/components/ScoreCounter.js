3c3deaa253dff0695df82c58ca1991d395a6d8b9
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-12 10:04:17 -0400

    deleted createCardOrder but not cardOrder'

--- src/components/ScoreCounter.js
+++ src/components/ScoreCounter.js
@@ -2,25 +2,7 @@ import React from "react";
 import MemoryCard from './MemoryCard';
 import '../ScoreCounter.css';
 function ScoreCounter (props) {
-    function createCardOrder() {
-        const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-        const newOrder = [];
-        /* To generate random numbers */
-        for(let i = 0; i < numberArray.length; i += 1) {
-            newOrder.push(Math.floor(Math.random() * 12 + 1));
-        }
-        /* To generate the rest of the numbers */
-        for(let i = 0; i < numberArray.length; i += 1) {
-            const index = newOrder.indexOf(numberArray[i]);
-            if(index === -1)
-            {
-                newOrder.push(numberArray[i]);
-            }
-        }
-        /* This is so we don't have any duplicates */
-        const uniqueOrder = [...new Set(newOrder)];
-        return uniqueOrder;
-    }
+    
     const cardOrder = createCardOrder();
     return <div>
         <div className="Card-row-1">
