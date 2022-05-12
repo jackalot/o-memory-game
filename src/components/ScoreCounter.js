6d5043d1f8b0de5d93b04926f4c1126bd37da11b
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-12 09:07:47 -0400

    added a for loop and a newOrder array

--- src/components/ScoreCounter.js
+++ src/components/ScoreCounter.js
@@ -3,6 +3,12 @@ import React from "react";
 function ScoreCounter (props) {
     function createCardOrder() {
         const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+        const newOrder = [];
+        /* To generate random numbers */
+        for(let i = 0; i < numberArray.length; i += 1) {
+            newOrder.push(Math.floor(Math.random() * 12));
+        }
+       
     }
 }
 export default ScoreCounter;
