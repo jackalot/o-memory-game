9c733e97e94b97e453395f5fd1e8563f8e732830
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-12 10:07:53 -0400

    deleted card Order, created numberArray, called shuffleOrder and passed NumberArray

--- src/components/ScoreCounter.js
+++ src/components/ScoreCounter.js
@@ -5,7 +5,8 @@ function ScoreCounter (props) {
     function shuffleOrder(numberArray) {
         numberArray.sort(() => Math.random() - 0.5);
     }
-    const cardOrder = createCardOrder();
+    const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    shuffleOrder(numberArray);
     return <div>
         <div className="Card-row-1">
         <MemoryCard number={cardOrder[0]}></MemoryCard>
