582912220b62ec6751cde4df1d263952200563c7
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-13 09:23:14 -0400

    in the on click attriblue, add 'props.handler'

--- src/components/MemoryCard.js
+++ src/components/MemoryCard.js
@@ -4,7 +4,7 @@ import '../MemoryCard.css';
 function MemoryCard (props) {
     const [clicked, setClicked] = useState(false);
     
-    return <div onClick={} className="card">
+    return <div onClick={props.handler} className="card">
         <div className="card-content">
             <p>{props.number}</p>
         </div>
