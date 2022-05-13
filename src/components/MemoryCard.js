b0133b1e5662fabfe01ea9ea18027ffbe696c43d
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-13 07:54:55 -0400

    created state for MemoryCard, added clicked state variable, added handleCLick function, added a onClick attribute for the div of className card

--- src/components/MemoryCard.js
+++ src/components/MemoryCard.js
@@ -1,8 +1,19 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import '../MemoryCard.css';
 
 function MemoryCard (props) {
-    return <div className="card">
+    const [clicked, setClicked] = useState(false);
+    function handleClick(){
+        if(clicked === false)
+        {
+            setClicked(true);
+        }
+        if(clicked === true)
+        {
+            setClicked(false);
+        }
+    }
+    return <div onClick={handleClick} className="card">
         <div className="card-content">
             <p>{props.number}</p>
         </div>
