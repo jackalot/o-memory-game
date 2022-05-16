20435adad4c0bdf77ca61169a2f69621475c6735
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-16 10:21:06 -0400

    moved the MemoryCards and divs from ScoreCounter to App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import MemoryCard from './components/MemoryCard';
 import ScoreCounter from './components/ScoreCounter';
 function App() {
     return <div>
-        <ScoreCounter></ScoreCounter>
+        <div className="Card-row-1">
+        <MemoryCard number={1}></MemoryCard>
+        <MemoryCard number={2}></MemoryCard>
+        <MemoryCard number={3}></MemoryCard>
+        <MemoryCard number={4}></MemoryCard>
+        <MemoryCard number={5}></MemoryCard>
+        <MemoryCard number={6}></MemoryCard>
+        </div>
+        <div className="Card-row-2">
+        <MemoryCard number={7}></MemoryCard>
+        <MemoryCard number={8}></MemoryCard>
+        <MemoryCard number={9}></MemoryCard>
+        <MemoryCard number={10}></MemoryCard>
+        <MemoryCard number={11}></MemoryCard>
+        <MemoryCard number={12}></MemoryCard>
+        </div>
     </div>
 }
 
--- src/components/ScoreCounter.js
+++ src/components/ScoreCounter.js
@@ -4,22 +4,6 @@ import '../ScoreCounter.css';
 function ScoreCounter (props) {
     
     return <div>
-        <div className="Card-row-1">
-        <MemoryCard number={1}></MemoryCard>
-        <MemoryCard number={2}></MemoryCard>
-        <MemoryCard number={3}></MemoryCard>
-        <MemoryCard number={4}></MemoryCard>
-        <MemoryCard number={5}></MemoryCard>
-        <MemoryCard number={6}></MemoryCard>
-        </div>
-        <div className="Card-row-2">
-        <MemoryCard number={7}></MemoryCard>
-        <MemoryCard number={8}></MemoryCard>
-        <MemoryCard number={9}></MemoryCard>
-        <MemoryCard number={10}></MemoryCard>
-        <MemoryCard number={11}></MemoryCard>
-        <MemoryCard number={12}></MemoryCard>
-        </div>
     </div>
 }
 export default ScoreCounter;
