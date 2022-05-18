63c1fa5e78abb03e39915aab24661d89f13af704
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-18 10:20:07 -0400

    added some elements to ScoreCounter, and props

--- src/components/ScoreCounter.js
+++ src/components/ScoreCounter.js
@@ -3,7 +3,13 @@ import MemoryCard from './MemoryCard';
 import '../ScoreCounter.css';
 function ScoreCounter (props) {
     
-    return <div>
+    return <div className="scoreBoard">
+       <div className="currentScore">
+        <p>{props.currentScore}</p>
+       </div>
+       <div className="highScore">
+        <p>{props.highScore}</p>
+       </div>
     </div>
 }
 export default ScoreCounter;
