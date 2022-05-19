f7e4d07fcd35b96fba5c89392ab7cd1a3394fd9e
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-19 07:35:18 -0400

    added a useEffect hook where if the current score is higher than the high schore, the high score is set to the current score

--- src/components/ScoreCounter.js
+++ src/components/ScoreCounter.js
@@ -2,7 +2,12 @@ import React, { useEffect, useState } from "react";
 import '../ScoreCounter.css';
 function ScoreCounter (props) {
     const [highScore, setHighScore] = useState(0);
-    
+    useEffect(() => {
+        if(props.currentScore > highScore)
+        {
+            setHighScore(props.currentScore);
+        }
+    })
     return <div className="scoreBoard">
        <div className="currentScore">
         <p>Current Score: {props.currentScore}</p>
